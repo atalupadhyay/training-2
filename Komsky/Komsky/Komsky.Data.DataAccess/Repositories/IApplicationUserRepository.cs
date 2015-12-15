513e50188febcad6dcc72c083f0b2177dff98340
Adrian Ilewicz <komsky@gmail.com>
2015-12-15 12:00:03 +0000

    LAB01 - Task 03

---
+++ Komsky/Komsky/Komsky.Data.DataAccess/Repositories/IApplicationUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Komsky.Data.Entities;
+
+namespace Komsky.Data.DataAccess.Repositories
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+        ApplicationUser GetByEmail(string email);
+    }
+}
