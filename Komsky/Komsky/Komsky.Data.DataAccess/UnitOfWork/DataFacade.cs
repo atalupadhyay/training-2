917a35cd8677bc80d8045d186d17c102e77ef53c
Adrian Ilewicz <komsky@gmail.com>
2015-12-15 12:28:28 +0000

    LAB 01 - Task 04

---
+++ Komsky/Komsky/Komsky.Data.DataAccess/UnitOfWork/DataFacade.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Komsky.Data.DataAccess.Repositories;
+
+namespace Komsky.Data.DataAccess.UnitOfWork
+{
+    public class DataFacade : IDataFacade
+    {
+        #region Fields
+        private ApplicationDbContext _dbContext;
+        private ApplicationUserRepository _applicationUsers;
+        #endregion
+
+        #region Constructors
+        public DataFacade()
+        {
+            CreateDbContext(null);
+        }
+
+        public DataFacade(ApplicationDbContext dbContext)
+        {
+            CreateDbContext(dbContext);
+        }
+
+        protected void CreateDbContext(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? new ApplicationDbContext();
+
+            _dbContext.Configuration.ProxyCreationEnabled = false;
+            _dbContext.Configuration.LazyLoadingEnabled = false;
+            _dbContext.Configuration.ValidateOnSaveEnabled = false;
+        }
+        #endregion
+
+        public ApplicationUserRepository ApplicationUsers
+        {
+            get { return _applicationUsers ?? (_applicationUsers = new ApplicationUserRepository(_dbContext)); }
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public Task CommitAsync()
+        {
+            return _dbContext.SaveChangesAsync();
+        }
+
+        #region Dispose pattern
+        private bool _disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+            }
+
+            _disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ Komsky/Komsky/Komsky.Data.DataAccess/UnitOfWork/IDataFacade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Komsky.Data.DataAccess.Repositories;
+
+namespace Komsky.Data.DataAccess.UnitOfWork
+{
+    public interface IDataFacade : IDisposable
+    {
+        void Commit();
+        Task CommitAsync();
+        ApplicationUserRepository ApplicationUsers { get; }
+    }
+}
