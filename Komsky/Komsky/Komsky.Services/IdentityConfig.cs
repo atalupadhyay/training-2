4c3c73d16c1eb3c4cebb93c9cd5684ef0206ea93
Adrian Ilewicz <komsky@gmail.com>
2015-12-15 15:03:19 +0000

    LAB 01 - Task 05

--- Komsky/Komsky/Komsky.Services/IdentityConfig.cs
+++ Komsky/Komsky/Komsky.Services/IdentityConfig.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
 using Komsky.Data.DataAccess;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -15,6 +11,7 @@ using Komsky.Data.Entities;
 
 namespace Komsky.Web
 {
+
     public class EmailService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
@@ -33,7 +30,6 @@ namespace Komsky.Web
         }
     }
 
-    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
--- Komsky/Komsky/Komsky.Web/Controllers/HomeController.cs
+++ Komsky/Komsky/Komsky.Web/Controllers/HomeController.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Komsky.Data.DataAccess.UnitOfWork;
 
 namespace Komsky.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IDataFacade _dataFacade;
+
+        public HomeController(IDataFacade dataFacade)
+        {
+            _dataFacade = dataFacade;
+        }
+
+
         public ActionResult Index()
         {
+
+            if (User != null && User.Identity != null && !String.IsNullOrEmpty(User.Identity.Name))
+            {
+                ViewBag.UserDetails = _dataFacade.ApplicationUsers.GetByEmail(User.Identity.Name).Email;
+            }
             return View();
         }
 
