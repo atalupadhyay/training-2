b9c702085c14cb4765dae6bcc013469d7e7b1ea7
Adrian Ilewicz <komsky@gmail.com>
2015-12-27 23:55:56 +0000

    LAB 05 B - Task 03 Solution

--- Komsky/Komsky/Komsky.Data.DataAccess/ApplicationDbContext.cs
+++ Komsky/Komsky/Komsky.Data.DataAccess/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public virtual DbSet<Customer> Customers { get; set; }
     public virtual DbSet<Product> Products { get; set; }
     public virtual DbSet<Ticket> Tickets { get; set; }
+    public DbSet<SystemLog> SystemLogs { get; set; }
     public ApplicationDbContext()
         : base("DefaultConnection", throwIfV1Schema: false)
     {
