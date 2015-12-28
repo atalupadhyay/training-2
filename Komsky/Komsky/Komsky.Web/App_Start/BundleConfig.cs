4b9b8de8388039abb59f9aa86d791da0d44446db
Adrian Ilewicz <komsky@gmail.com>
2015-12-28 04:24:15 +0000

    LAB 07 - Solution with error

--- Komsky/Komsky/Komsky.Web/App_Start/BundleConfig.cs
+++ Komsky/Komsky/Komsky.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Komsky.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.easing.{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,14 +25,21 @@ namespace Komsky.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/site.css",
+                        "~/Content/font-awesome.css",
+                        "~/Content/Theme/animate.min.css",
+                        "~/Content/Theme/creative.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/global").Include(
                         "~/Scripts/global.js",
                         "~/Scripts/init.js"));
+            bundles.Add(new ScriptBundle("~/bundles/theme").Include(
+            "~/Scripts/Theme/jquery.fittext.js",
+            "~/Scripts/Theme/wow.min.js",
+            "~/Scripts/Theme/creative.js"));
 
-            BundleTable.EnableOptimizations = true;
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
