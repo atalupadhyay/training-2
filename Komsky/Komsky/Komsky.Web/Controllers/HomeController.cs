f63d21e433aae3c07fd934db1c5ae0a2b8e79607
unknown <komsky@gmail.com>
2016-02-21 12:14:13 +0100

    Temp commit

--- Komsky/Komsky/Komsky.Web/Controllers/HomeController.cs
+++ Komsky/Komsky/Komsky.Web/Controllers/HomeController.cs
@@ -1,13 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Threading;
+using System.Web.Mvc;
 
 namespace Komsky.Web.Controllers
 {
     public partial class HomeController : Controller
     {
-       public virtual ActionResult Index()
+        public virtual ActionResult Index()
         {
+            if (HttpContext.Cache["CurrentTime"] == null)
+            {
+                HttpContext.Cache.Insert("CurrentTime", ReadServerTime());
+            }
+            ViewBag.CurrentTime = HttpContext.Cache["CurrentTime"].ToString();
             ViewBag.Title = "Home";
             return View();
+        }
+
+        private string ReadServerTime()
+        {
+            Thread.Sleep(10000);
+            return DateTime.Now.ToLongTimeString();
         }
 
         public virtual ActionResult About()
