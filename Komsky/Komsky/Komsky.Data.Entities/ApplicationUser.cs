2900fae46abd0e298c60746bd3a28a5fcd56773b
unknown <komsky@gmail.com>
2016-02-21 14:41:00 +0100

    Temp commit Slides update Workbook update Lab 09 idea updated

--- Komsky/Komsky/Komsky.Data.Entities/ApplicationUser.cs
+++ Komsky/Komsky/Komsky.Data.Entities/ApplicationUser.cs
@@ -7,24 +7,12 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Komsky.Data.Entities
 {
-    public class ApplicationUser : IUser<string>
+    public class ApplicationUser : IdentityUser
     {
         [ForeignKey("Customer")]
         public int? CustomerId { get; set; }
         public Customer Customer { get; set; }
 
-        public string Id { get; set; }
-        public string UserName { get; set; }
-        public String Email { get; set; }
-        public bool EmailConfirmed { get; set; }
-        public String PasswordHash { get; set; }
-        public String SecurityStamp { get; set; }
-        public String PhoneNumber { get; set; }
-        public bool PhoneNumberConfirmed { get; set; }
-        public bool TwoFactorEnabled { get; set; }
-        public bool LockoutEnabled { get; set; }
-        public DateTime? LockoutEndDateUtc { get; set; }
-        public int AccessFailedCount { get; set; }
     }
 
 }
