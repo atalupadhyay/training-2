051ba2673792cfc45d95c605371304208ab6411f
unknown <komsky@gmail.com>
2016-02-28 09:39:26 +0100

    LAB 09 A - Task 01 Solution

--- Komsky/Komsky/Komsky.Data.DataAccess/Migrations/Configuration.cs
+++ Komsky/Komsky/Komsky.Data.DataAccess/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace Komsky.Data.DataAccess.Migrations
 {
+    using Komsky.Data.Entities;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +17,46 @@ namespace Komsky.Data.DataAccess.Migrations
 
         protected override void Seed(Komsky.Data.DataAccess.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>();
+            RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(context);
+            UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(userStore);
+
+            if (!(context.Roles.Any(u => u.Name == "Admin")))
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+
+            if (!(context.Roles.Any(u => u.Name == "Agent")))
+            {
+                roleManager.Create(new IdentityRole { Name = "Agent" });
+            }
+            if (!(context.Roles.Any(u => u.Name == "User")))
+            {
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+
+
+            if (!(context.Users.Any(u => u.UserName == "admin@komsky.com")))
+            {
+                var userToInsert = new ApplicationUser { UserName = "admin@komsky.com", Email = "admin@komsky.com", PhoneNumber = "07456789456" };
+                var result = userManager.Create(userToInsert, "Pa$$w0rd");
+                if (!result.Errors.Any())
+                {
+                    userManager.AddToRole(userToInsert.Id, "Admin");
+                }
+            }
+
+            if (!(context.Users.Any(u => u.UserName == "agent@komsky.com")))
+            {
+                var userToInsert = new ApplicationUser { UserName = "agent@komsky.com", Email = "agent@komsky.com", PhoneNumber = "07456789456" };
+                var result = userManager.Create(userToInsert, "Pa$$w0rd");
+                if (!result.Errors.Any())
+                {
+                    userManager.AddToRole(userToInsert.Id, "Agent");
+                }
+            }
         }
     }
 }
--- Komsky/Komsky/Komsky.Web/Controllers/AccountController.cs
+++ Komsky/Komsky/Komsky.Web/Controllers/AccountController.cs
@@ -149,30 +149,24 @@ namespace Komsky.Web.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public virtual async Task<ActionResult> Register(RegisterViewModel model)
+public virtual async Task<ActionResult> Register(RegisterViewModel model)
+{
+    if (ModelState.IsValid)
+    {
+        var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+        var result = await UserManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
         {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+            await UserManager.AddToRoleAsync(user.Id, "User");
+            await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
 
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
+            return RedirectToAction("Index", "Home");
         }
+        AddErrors(result);
+    }
+
+    return View(model);
+}
 
         //
         // GET: /Account/ConfirmEmail
